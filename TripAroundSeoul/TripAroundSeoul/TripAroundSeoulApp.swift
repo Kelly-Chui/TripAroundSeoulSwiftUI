d2fdb8065769d34728cb00a8428268aa9b1aabcf
Kelly-Chui <optioneldar@gmail.com>
2022-08-30 09:48:39 +0900

    Create Xcode Project

---
+++ TripAroundSeoul/TripAroundSeoul/TripAroundSeoulApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripAroundSeoulApp.swift
+//  TripAroundSeoul
+//
+//  Created by Kelly Chui on 2022/08/30.
+//
+
+import SwiftUI
+
+@main
+struct TripAroundSeoulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
